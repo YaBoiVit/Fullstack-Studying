58a4efc6bb496afe4659d2499c64836c129ed727
YaBoiVit <vitali22222@gmail.com>
2022-09-27 20:41:30 +0300

    Fixes, nearly done

--- Project Vitali/index.js
+++ Project Vitali/index.js
@@ -1,8 +1,7 @@
 var Saved = []
 if(localStorage.Saved){
     Saved=JSON.parse(localStorage.Saved)
-    PrintSavedNotes()
-    var k = Saved.length
+    createSticky(Saved)
 }
 function SaveTextArea(){
     var container = {
@@ -17,35 +16,29 @@ function SaveTextArea(){
 }
 function createSticky(Saved){
     console.log(Saved)
-    console.log(k)
-    var postnotes = `<div class="stickynote col-2" id="stickynote${k}">
-        <button type="button" class="btn-close" onclick="CloseNote(${k})"aria-label="Close"></button>
-    <p class="stickytask"> ${Saved[k].Task} </p>
-    <p class="stickydate"> ${Saved[k].Date} </p><br/>
-    <p class="stickytime"> ${Saved[k].Time} </p>
-    </div>`
-    notesRow.innerHTML += postnotes
-    k++;
-}
-function ResetTextArea() {
-    formEL.reset();
-}
-function CloseNote(num) {
-Saved.splice(num,1)
-localStorage.setItem("Saved", JSON.stringify(Saved))
-document.getElementById(`stickynote${num}`).remove();
-console.log(Saved)
-}
-function PrintSavedNotes() {
-notesRow.innerHTML = ''
-for (var i = 0; i < Saved.length; i++) {
-    var postnotes = `<div class="stickynote col-2" id="stickynote${i}">
+    notesRow.innerHTML = ''
+    for (var i = 0; i < Saved.length; i++) {
+    var postnotes = `<div class="stickynote col-1">
         <button type="button" class="btn-close" onclick="CloseNote(${i})"aria-label="Close"></button>
     <p class="stickytask"> ${Saved[i].Task} </p>
     <p class="stickydate"> ${Saved[i].Date} </p><br/>
     <p class="stickytime"> ${Saved[i].Time} </p>
     </div>`
     notesRow.innerHTML += postnotes
+}
+}
+function ResetTextArea() {
+    formEL.reset();
+}
+function ResetNotes(){
+    notesRow.innerHTML = '';
+    while (Saved.length)
+    var poppedElement = Saved.pop();
+    localStorage.setItem("Saved", JSON.stringify(Saved))
     console.log(Saved)
 }
+function CloseNote(num) {
+Saved.splice(num,1)
+localStorage.setItem("Saved", JSON.stringify(Saved))
+createSticky(Saved)
 }
